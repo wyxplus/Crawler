51aecd74bd455fbc6c81a32a2ae1da27b7afa501
wyxplus <32919297+wyxplus@users.noreply.github.com>
2021-05-28 14:57:13 +0800

    Add files via upload

---
+++ 新闻联播文字版/setting.py
@@ -0,0 +1,20 @@
+# headers = {
+#     "authority": "www.zhihu.com",
+#     "method": "POST",
+#     "scheme": "https",
+#     "accept-language": "zh-CN,zh;q=0.9",
+#     "cookie": 'device_id=24700f9f1986800ab4fcc880530dd0ed; xq_a_token=385b836a045da45667afda72237fc969313f56f0; xqat=385b836a045da45667afda72237fc969313f56f0; xq_r_token=f04bf9be3f04ead615a88752d56293c4ae5eec0b; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOi0xLCJpc3MiOiJ1YyIsImV4cCI6MTYyMzgxMTc5NywiY3RtIjoxNjIxODIwMDgwNjAyLCJjaWQiOiJkOWQwbjRBWnVwIn0.qzLdFlS2T794ELR2Rdl0h9GmFSFPhXKmafyeRdy78ZjUZw-9icWagd_Ap55ZROlWVWZw5HSA8_PrWS6m8iq2ZTvG-y8oLXzpk9MzR37pCPxkph4zlNLGD3Dz0a-ageXI26np3VBG2VidtzBYWa3HO3QJrPWbV4UxNi63qNbAGlp7c_egddq66NhauQgMfEfsAuKK-QPp-XAaErUsJBVH2Y-5IXV_tqgjJSwGD3eDFkVRqvNzKNjzku4vT5dZadUVtBp89mtp5BSOZHPp7v67u2YKsa68wlWFn2tsO6z3aOCfn6AanRCO8zJ87iyL28YJJp4pCH_VYLh8qfr8jcP6Vw; u=861621820119806',
+#     "origin": "https://xueqiu.com",
+#     "referer": "https://xueqiu.com/?category=livenews",
+#     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+#     "x-requested-with": "XMLHttpRequest",
+#     "x-xsrftoken": "iqWraCpzOAEVVHNZGwDfyaUPzBb7lkuI",
+#     "_xsrf":"697157726143707a4f41455656484e5a47774466796155507a4262376c6b7549"
+# }
+
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+}
+
+url = r"http://mrxwlb.com/category/mrxwlb-text/page/" # + pageNumber
